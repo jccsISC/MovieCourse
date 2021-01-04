99e3ef33b525d18553ffa6e5bc1ac98640e2b6e3
jccsISC <silva.jccs18@gmail.com>
2021-01-04 16:27:25 -0600

    agregando Dao

---
+++ app/src/main/java/com/jccsisc/jpelisc/data/local/MovieDao.kt
@@ -0,0 +1,18 @@
+package com.jccsisc.jpelisc.data.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.jccsisc.jpelisc.data.model.MovieEntity
+
+@Dao
+interface MovieDao {
+
+    @Query("SELECT * FROM movieentity")
+    suspend fun getAllMovies(): List<MovieEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE) //si ya existe el elemento lo reemplaza
+    suspend fun saveMovie(movieEntity: MovieEntity)
+
+}
